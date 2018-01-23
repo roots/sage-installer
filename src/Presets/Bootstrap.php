0b14debb5efa8a61bf45db58dc07f93cfa57f897
Stefan Lindberg <stefan@wonderboys.se>
2018-01-23 18:22:28 +0100

    Update Bootstrap preset for Bootstrap release version 4.0.0

--- src/Presets/Bootstrap.php
+++ src/Presets/Bootstrap.php
@@ -7,8 +7,8 @@ class Bootstrap extends Preset
     /** {@inheritdoc} */
     protected function updatePackagesArray(array $packages)
     {
-        $packages['dependencies']['bootstrap'] = 'v4.0.0-beta.2';
-        $packages['dependencies']['popper.js'] = '^1.12.3';
+        $packages['dependencies']['bootstrap'] = 'v4.0.0';
+        $packages['dependencies']['popper.js'] = '^1.12.9';
         return $packages;
     }
 }
