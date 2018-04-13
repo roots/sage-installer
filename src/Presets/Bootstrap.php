5966e0ea56b46f50f183f7ad0e526ddf065265fb
Ben Word <ben@benword.com>
2018-04-12 19:32:58 -0600

    Bootstrap 4.1.0

--- src/Presets/Bootstrap.php
+++ src/Presets/Bootstrap.php
@@ -7,8 +7,8 @@ class Bootstrap extends Preset
     /** {@inheritdoc} */
     protected function updatePackagesArray(array $packages)
     {
-        $packages['dependencies']['bootstrap'] = 'v4.0.0';
-        $packages['dependencies']['popper.js'] = '^1.12.9';
+        $packages['dependencies']['bootstrap'] = 'v4.1.0';
+        $packages['dependencies']['popper.js'] = '^1.14.3';
         return $packages;
     }
 }
