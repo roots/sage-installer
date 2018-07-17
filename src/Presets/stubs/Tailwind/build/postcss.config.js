8ffbb4b65c6310cd4ce8d80898453238731b3140
Nathan Knowler <nathan@knowler.me>
2018-07-17 02:51:38 -0600

    Add Tailwind preset's PostCSS config

---
+++ src/Presets/stubs/Tailwind/build/postcss.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+const cssnanoConfig = {
+  preset: ['default', { discardComments: { removeAll: true } }]
+};
+
+module.exports = ({ file, options }) => {
+  return {
+    parser: options.enabled.optimize ? 'postcss-safe-parser' : undefined,
+    plugins: {
+      tailwindcss: `${options.paths.assets}/styles/tailwind.config.js`,
+      autoprefixer: true,
+      cssnano: options.enabled.optimize ? cssnanoConfig : false,
+    },
+  };
+};
