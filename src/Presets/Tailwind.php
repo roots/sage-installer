1a039fab2c56284d6ab94fe2490563053e3a0f73
Matt Mirus <matt@mattmirus.com>
2018-11-26 19:54:28 -0500

    Remove reference to package.json stylelint config

--- src/Presets/Tailwind.php
+++ src/Presets/Tailwind.php
@@ -9,16 +9,6 @@ class Tailwind extends Preset
     {
         $packages['devDependencies']['tailwindcss'] = '^0.6.5';
 
-        /** Add Tailwind specific at-rules */
-        $ignoreAtRules =& $packages['stylelint']['rules']['at-rule-no-unknown'][1]['ignoreAtRules'];
-        $tailwindAtRules = ['tailwind', 'apply', 'responsive', 'variants', 'screen'];
-
-        foreach ($tailwindAtRules as $rule) {
-            if (!in_array($rule, $ignoreAtRules)) {
-                $ignoreAtRules[] = $rule;
-            }
-        }
-
         return $packages;
     }
 }
