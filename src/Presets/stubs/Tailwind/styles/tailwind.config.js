d3f2a128653934923a3f3b3e4f1cca9fdf9ea465
Nathan Knowler <nathan@knowler.me>
2019-05-09 23:57:52 -0600

    Update colour names + Tailwind package version

--- src/Presets/stubs/Tailwind/styles/tailwind.config.js
+++ src/Presets/stubs/Tailwind/styles/tailwind.config.js
@@ -11,6 +11,19 @@ module.exports = {
   theme: {
     colors: {
       primary: '#525ddc',
+      white: '#fff',
+      gray: {
+        100: '#f7fafc',
+        200: '#edf2f7',
+        300: '#e2e8f0',
+        400: '#cbd5e0',
+        500: '#a0aec0',
+        600: '#718096',
+        700: '#4a5568',
+        800: '#2d3748',
+        900: '#1a202c',
+      },
+      transparent: 'transparent',
     },
     shadows: {
       outline: '0 0 0 3px rgba(82,93,220,0.3)',
