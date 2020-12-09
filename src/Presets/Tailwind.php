28bb69a601d336e881f98a047bb43e17f0accf35
Nathan Knowler <nathan@knowler.me>
2020-12-09 15:54:09 -0600

    Add Tailwind 2 peer deps to package.json

--- src/Presets/Tailwind.php
+++ src/Presets/Tailwind.php
@@ -8,6 +8,8 @@ class Tailwind extends Preset
     protected function updatePackagesArray(array $packages)
     {
         $packages['devDependencies']['tailwindcss'] = '^2.0.1';
+        $packages['devDependencies']['postcss'] = '^8.0.9';
+        $packages['devDependencies']['autoprefixer'] = '^10.0.2';
 
         return $packages;
     }
