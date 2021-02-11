b7c14a0cb3f2a9790aa21518c820305d29912258
strarsis <strarsis@gmail.com>
2021-02-11 10:04:40 +0100

    Fix indentation.

--- src/Presets/Bootstrap5.php
+++ src/Presets/Bootstrap5.php
@@ -8,7 +8,7 @@ class Bootstrap5 extends Preset
     protected function updatePackagesArray(array $packages)
     {
         $packages['dependencies']['bootstrap'] = 'v5.0.0-beta2'; // 5.x currently in beta
-		// (popper.js already included)
+        // (popper.js already included)
 
         return $packages;
     }
