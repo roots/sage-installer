5eca069442c17d22c90cb85d5196d2af0d1a86e7
strarsis <strarsis@gmail.com>
2021-06-02 16:04:52 +0200

    Update Bootstrap 5 bootstrap dependency to stable 5.x.

--- src/Presets/Bootstrap5.php
+++ src/Presets/Bootstrap5.php
@@ -7,7 +7,7 @@ class Bootstrap5 extends Preset
     /** {@inheritdoc} */
     protected function updatePackagesArray(array $packages)
     {
-        $packages['dependencies']['bootstrap'] = 'v5.0.0-beta2'; // 5.x currently in beta
+        $packages['dependencies']['bootstrap'] = 'v5.0.1';
         // (popper.js already included)
 
         return $packages;
