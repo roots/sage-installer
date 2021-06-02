1e2c410053d7f8694c282891fe183587442c0b36
strarsis <strarsis@gmail.com>
2021-06-02 16:15:19 +0200

    Update Bootstrap 4 bootstrap and popper dependencies to latest releases.

--- src/Presets/Bootstrap4.php
+++ src/Presets/Bootstrap4.php
@@ -7,8 +7,8 @@ class Bootstrap4 extends Preset
     /** {@inheritdoc} */
     protected function updatePackagesArray(array $packages)
     {
-        $packages['dependencies']['bootstrap'] = 'v4.3.1';
-        $packages['dependencies']['popper.js'] = '^1.14.7';
+        $packages['dependencies']['bootstrap'] = 'v4.6.0';
+        $packages['dependencies']['popper.js'] = '^1.16.1';
         return $packages;
     }
 }
